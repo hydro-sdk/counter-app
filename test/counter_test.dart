b05e2fde0ff56042df5a85393ed1e0e662a57a27
Chris Gibb <chris.gibb@outlook.com>
2021-03-23 17:43:24 -0400

    rename test and update ignore

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -4,26 +4,27 @@ import 'package:hydro_sdk/runComponent/runComponent.dart';
 
 void main() {
   LiveTestWidgetsFlutterBinding();
-  testWidgets('example project smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(RunComponentFromFile(
+  testWidgets('counter smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(const RunComponentFromFile(
       component: "counter-example",
       path: "../assets/ota/counter-example.ota",
     ));
+
     await tester.pumpAndSettle();
 
     var exception = tester.takeException();
     expect(exception, isNull);
 
-    expect(find.byKey(Key("counter")), findsOneWidget);
-    expect(find.byKey(Key("increment")), findsOneWidget);
+    expect(find.byKey(const Key("counter")), findsOneWidget);
+    expect(find.byKey(const Key("increment")), findsOneWidget);
     expect(find.text("You have pushed the button this many times"),
         findsOneWidget);
 
     expect(find.text("0"), findsOneWidget);
-    await tester.tap(find.byKey(Key("increment")));
+    await tester.tap(find.byKey(const Key("increment")));
     await tester.pumpAndSettle();
     expect(find.text("1"), findsOneWidget);
-    await tester.tap(find.byKey(Key("increment")));
+    await tester.tap(find.byKey(const Key("increment")));
     await tester.pumpAndSettle();
     expect(find.text("2"), findsOneWidget);
   });
